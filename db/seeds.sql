8158698ca38cee8c0e5ad460ffae8ae219ebec2e
Olivia Oxley <ooxley918@gmail.com>
2019-03-10 19:11:05 -0700

    finished assignment + styling

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portbello Mushroom Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
